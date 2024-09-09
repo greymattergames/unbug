95193e12bacea1300e2d3f33a84192f202885bb9
Brian Jesse <brian.jesse@gmail.com>
2024-09-09 17:48:50 +0200

    init

---
+++ README.md
@@ -0,0 +1,120 @@
+# Unbug
+
+A crate to programmatically invoke debugging breakpoints with helping macros.
+
+These macros are designed to help developers catch errors during debugging sessions that would otherwise be a panic (which may not be desirable in certain contexts) or simply a log message (which may go unnoticed).
+
+This crate's internals are disabled by default, there are shims provided so breakpoints will not be compiled outside of a debugging context. This means that the macros in this crate can be used freely throughout your code without having to conditionally compile them out yourself.
+
+> ### NOTICE
+>
+> You must use the `enable` feature of this crate (deactivated by default) to activate the breakpoints. This crate cannot detect the presence of a debugger.
+>
+> __BREAKPOINTS REQUIRE NIGHTLY RUST__
+>
+> __BREAKPOINTS REQUIRE ENABLING THE EXPERIMENTAL [`core_intrinsics`](https://doc.rust-lang.org/core/intrinsics/fn.breakpoint.html) FEATURE__
+>
+> Additonally, debugging may not land on the macro statements themselves. This can have the consequence that the debgger may pause on an internal module. To avoid this, `return` or `continue` immediately following a macro invocation. Alternatively, use your debugger's "step-out" feature until you reenter the scope of your code.
+
+## Examples
+
+```rust
+// trigger the debugger
+unbug::breakpoint!();
+
+let some_bool = false;
+
+for i in 0..5 {
+    // ensure! will only trigger the debugger once when the expression argument is false
+    unbug::ensure!(some_bool);
+
+    // ensure_always! will trigger the debugger every time
+    unbug::ensure_always!(some_bool);
+}
+
+let my_var: Option<()> = None;
+
+// the `fail!` macro compliments the guards pattern
+let Some(out_var) = my_var else {
+    // fail! pauses and logs an error message, will also only trigger once
+    // fail! will continue to log in non-debug builds
+    unbug::fail!("failed to get some option");
+    return;
+};
+
+// the `fail!` macro compliments the guards pattern
+let Some(other_out_var) = my_var else {
+    // fail_always! will do the same, but will trigger every time
+    unbug::fail_always!("failed to get some option");
+    return;
+};
+```
+
+## Installation
+
+__1.__ Enable Nightly Rust:
+```bash
+rustup install nightly
+rustup default nightly
+```
+
+__2.__ Create a debug feature in your project that will only be active in the context of a debugger, i.e. not enabled by default.
+
+`Cargo.toml`:
+```toml
+[features]
+default = []
+my_debug_feature = [
+    "unbug/enable"
+]
+```
+
+__3.__ enable the core_intrinsics feature in the root of your crate (`main.rs` or `lib.rs`)
+
+`main.rs`:
+```rust
+#![cfg_attr(all(debug_assertions, feature = "my_debug_feature"), feature(core_intrinsics))]
+```
+> *this configuration will only activate core_intrinsics for debug builds when your debug feature is active*
+
+__4.__ Pass your feature flag to cargo during your debug build.
+
+Sample VSCode `launch.json` with LLDB:
+```json
+{
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "type": "lldb",
+            "request": "launch",
+            "name": "Debug",
+            "cargo": {
+                "args": [
+                    "build",
+                    "--bin=my_project",
+                    "--package=my_project",
+                    "--features=my_debug_feature"
+                ],
+                "filter": {
+                    "name": "my_project",
+                    "kind": "bin"
+                }
+            },
+            "args": [],
+            "cwd": "${workspaceFolder}",
+            "env": {
+                "CARGO_MANIFEST_DIR": "${workspaceFolder}"
+            }
+        }
+    ]
+}
+```
+
+## License
+
+Unbug is free and open source. All code in this repository is dual-licensed under either:
+
+- MIT License ([LICENSE-MIT](/LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
+- Apache License, Version 2.0 ([LICENSE-APACHE](/LICENSE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0>)
+
+at your option.
---
+++ src/lib.rs
@@ -0,0 +1,171 @@
+#![doc = include_str!("../README.md")]
+
+#[doc(hidden)]
+pub mod _internal {
+
+    // once implementation from bevy_utils (bevyengine.org), also dual-licensed under the MIT and Apache licenses
+    #[doc(hidden)]
+    #[macro_export]
+    macro_rules! _internal_once {
+        ($expression:expr) => {{
+            use ::std::sync::atomic::{AtomicBool, Ordering};
+
+            static SHOULD_FIRE: AtomicBool = AtomicBool::new(true);
+            if SHOULD_FIRE.swap(false, Ordering::Relaxed) {
+                $expression;
+            }
+        }};
+    }
+
+    #[doc(hidden)]
+    pub use tracing::error as _error;
+
+    // macro_export will move the macro to the root module, thus it is necessary to use `super::` here
+    #[doc(hidden)]
+    pub use super::_internal_once as _once;
+}
+
+/// When enabled, will pause execution with a break point
+///
+/// Requires Nightly Rust
+///
+/// # Examples
+///
+/// ```rust
+/// unbug::breakpoint!();
+/// ```
+#[macro_export]
+#[cfg(not(all(debug_assertions, feature = "enable")))]
+macro_rules! breakpoint {
+    () => {};
+}
+#[macro_export]
+#[cfg(all(debug_assertions, feature = "enable"))]
+macro_rules! breakpoint {
+    () => {
+        unsafe {
+            std::intrinsics::breakpoint();
+        }
+    };
+}
+
+/// When enabled, will pause execution with a break point when expression is false
+///
+/// Will break on every execution
+///
+/// Requires Nightly Rust
+///
+/// # Examples
+///
+/// ```rust
+/// unbug::ensure_always!(false);
+/// ```
+#[macro_export]
+#[cfg(not(all(debug_assertions, feature = "enable")))]
+macro_rules! ensure_always {
+    ($expression:expr) => {};
+}
+#[macro_export]
+#[cfg(all(debug_assertions, feature = "enable"))]
+macro_rules! ensure_always {
+    ($expression:expr) => {
+        if !$expression {
+            unbug::breakpoint!();
+        }
+    };
+}
+
+/// When enabled, will pause execution with a break point when expression is false
+///
+/// Will only break once per program run
+///
+/// Requires Nightly Rust
+///
+/// # Examples
+///
+/// ```rust
+/// unbug::ensure!(false);
+/// ```
+#[macro_export]
+#[cfg(not(all(debug_assertions, feature = "enable")))]
+macro_rules! ensure {
+    ($expression:expr) => {};
+}
+#[macro_export]
+#[cfg(all(debug_assertions, feature = "enable"))]
+macro_rules! ensure {
+    ($expression:expr) => {
+        unbug::_internal::_once!(unbug::ensure_always!($expression))
+    };
+}
+
+/// When enabled, will log an error and pause execution
+///
+/// Will break on every execution
+///
+/// When disabled, will log an error
+///
+/// Will log on every execution
+/// 
+/// arguments are processed by tracing's error! macro
+///
+/// Requires Nightly Rust
+///
+/// # Examples
+///
+/// ```rust
+/// unbug::fail_always!("failed to do something");
+/// unbug::fail_always!("failed to do something: {:?}", some_var);
+/// ```
+#[macro_export]
+#[cfg(not(all(debug_assertions, feature = "enable")))]
+macro_rules! fail_always {
+    ($($arg:tt),+ $(,)?) => {
+        unbug::_internal::_error!($($arg),+);
+    };
+}
+#[macro_export]
+#[cfg(all(debug_assertions, feature = "enable"))]
+macro_rules! fail_always {
+    ($($arg:tt),+ $(,)?) => {
+        unbug::_internal::_error!($($arg),+);
+        unbug::breakpoint!();
+    };
+}
+
+/// When enabled, will log an error and pause execution
+///
+/// Will only break once per program run
+///
+/// When disabled, will log an error
+///
+/// Will log once per program run
+/// 
+/// arguments are processed by Tracing's error! macro
+///
+/// Requires Nightly Rust
+///
+/// Requires Tracing logging
+///
+/// # Examples
+///
+/// ```rust
+/// unbug::fail!("failed to do something");
+/// unbug::fail!("failed to do something: {:?}", some_var);
+/// ```
+#[macro_export]
+macro_rules! fail {
+    ($($arg:tt),+ $(,)?) => {
+        unbug::_internal::_once!(unbug::fail_always!($($arg),+));
+    };
+}
+
+pub mod prelude {
+    pub use super::{
+        breakpoint,
+        ensure,
+        ensure_always,
+        fail,
+        fail_always,
+    };
+}
